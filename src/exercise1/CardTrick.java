d0e7a9f0474bbfd8eb70e1ec0bd1cdde5e938b66
kau12174 <kau12174@sheridancollege.ca>
2023-01-28 00:57:56 -0500

    ICE 1 version 1 branch1 JASHAN

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -85,7 +85,7 @@ System.out.println("Enter the value you guessed of card from 1 to 13  ");
      */
     
     
-    // "I AM DONE!!!"
+    // "I AM DONE !!!!!"
     private static void printInfo() {
     
         System.out.println("Congratulations,your guess was right!");
